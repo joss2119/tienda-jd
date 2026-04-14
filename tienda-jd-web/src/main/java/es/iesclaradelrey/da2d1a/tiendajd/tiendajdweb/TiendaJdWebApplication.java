0ee696f625e56efe0e7143b7ac9e76ade872aab7
José David <josedavidalviares@gmail.com>
2026-04-14 11:50:43 +0200

    Estructura base de modulos y pom.xml

---
+++ tienda-jd-web/src/main/java/es/iesclaradelrey/da2d1a/tiendajd/tiendajdweb/TiendaJdWebApplication.java
@@ -0,0 +1,13 @@
+package es.iesclaradelrey.da2d1a.tiendajd.tiendajdweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJdWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaJdWebApplication.class, args);
+    }
+
+}
---
+++ tienda-jd-web/src/test/java/es/iesclaradelrey/da2d1a/tiendajd/tiendajdweb/TiendaJdWebApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iesclaradelrey.da2d1a.tiendajd.tiendajdweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaJdWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
